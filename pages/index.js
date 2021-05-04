5de0957af81af841d9f452849d80f0f96c328f62
Camilo Caroso <finished92for@gmail.com>
2021-05-04 19:19:44 -0400

    sef

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home({message}) {
 }
 
 
-export const getStaticProps = async () => {
+export const getServerSideProps = async () => {
   const res = await fetch(`http://ec2-3-137-209-79.us-east-2.compute.amazonaws.com/api`)
   const message = await res.json()
   return {
